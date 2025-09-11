3e2f1e510bd44134e742b9ead09333a7189520f6
sn489yw <simonnigut@gmail.com>
2025-09-11 12:42:52 +0200

    Added todo list of code

---
+++ checker.py
@@ -0,0 +1,13 @@
+URLS = [
+    "https://example.com",
+]
+
+# Run Playwright headless browser
+
+# Inject axe-core
+
+# Save results to json file
+
+# Display results in html report
+
+# (Optional) Save pdf file of results
